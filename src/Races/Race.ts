03d7b7b519c24c5d245941c9e7cf7f40db8d29be
matheus proença <matheusproenca2020@hotmail.com>
2023-09-25 16:18:45 -0300

    Finalizando Req01

---
+++ src/Races/Race.ts
@@ -0,0 +1,27 @@
+abstract class Race {
+  private names: string
+  private dex: number
+
+  constructor(
+    name: string,
+    dexterity: number,
+  ) {
+    this.names = name;
+    this.dex = dexterity;
+  }
+
+  public get name(): string {
+    return this.names
+  }
+
+  public get dexterity(): number {
+    return this.dex
+  }
+
+  static createdRacesInstances(): number {
+    throw new Error("Not implemented")
+  }
+  abstract get maxLifePoints(): number;
+}
+
+export default Race
